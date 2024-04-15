7b7cdf549ac5e23ac5554c9e2f8a121ef7a5b039
propotyk123 <propotyk@gmail.com>
2024-04-15 13:38:31 +0200

    Połaczenie dwóch plików

--- Testpowtórzeniowy/Program.cs
+++ Testpowtórzeniowy/Program.cs
@@ -1,7 +1,14 @@
-﻿class PlikGłówny
-{ 
+﻿using Testpowtórzeniowy;
+
+class PlikGłówny
+{ //zmienna, która jest odwołaniem do innego skryptu
+    
     static void Main(string[] args)
     {
         //To jest baza naszego projektu
-    }
+        Tablice tab1 = new Tablice();
+
+        tab1.Wyswietl();
+    }//ja mu gaz a on mi zgasl made by karolina P.
+    
 }
---
+++ Testpowtórzeniowy/Tablice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testpowtórzeniowy
+{
+    public class Tablice
+    {
+        public void Wyswietl() 
+        {
+            Console.WriteLine("to jest plik");
+        }
+    }
+}
